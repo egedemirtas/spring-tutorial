2e70a1765c7ff47a96296abaaab02e02a79b5710
egedemirtas <egedemirtas@outlook.com>
2022-06-19 16:49:48 +0300

    post construct, pre destroy

--- spring-demo/src/main/java/com/egedemirtas/spring/framework/springdemo/basic/spring/MergeSortWithSpring.java
+++ spring-demo/src/main/java/com/egedemirtas/spring/framework/springdemo/basic/spring/MergeSortWithSpring.java
@@ -1,18 +1,34 @@
 package com.egedemirtas.spring.framework.springdemo.basic.spring;
 
 import com.egedemirtas.spring.framework.springdemo.basic.SortAlgo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.Comparator;
 import java.util.List;
 
 @Component
 @Qualifier("merge")
 public class MergeSortWithSpring implements SortAlgo {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     public List<Integer> sort(List<Integer> numberList) {
         // sort numberList
         numberList.sort(Comparator.naturalOrder()); // assume this is doing merge sort
         return numberList;
+    }
+
+    @PostConstruct
+    // as soon as bean is created and dependencies initialized this method will be called, thus you can perform actions that require autowired dependencies
+    public void postConstruct(){
+        logger.info("post construct");
+    }
+
+    @PreDestroy // just before destroying the bean and dependencies, this won't be called if your bean is prototype, you have to manually shutdown the application with code in main()
+    public void preDestroy(){
+        logger.info("pre destroy");
     }
 }
